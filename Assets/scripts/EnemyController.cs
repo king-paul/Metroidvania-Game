e369f577188d48ba764dae23d7b769dca9ecd381
Paul King <PaulKing09@bigpond.com>
2023-03-07 19:25:18 +1100

    Updated alien enemy to stop moving when reaching the edge of platform

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyController : MonoBehaviour
 {
     public int maxHealth = 5;
@@ -13,41 +15,55 @@ public class EnemyController : MonoBehaviour
     [Header("Enemy Fire")]
     public GameObject bullet;
     public float fireRate = 1.0f;
-    public float bulletSpawnOffsetDistance = 1.0f;
+    public float offsetDistance = 1.0f;
 
     private int health;
     private Transform player;
     private Rigidbody2D rb;
-
     private bool hasFired = false;
-   
+
+    private GameManager gameManager;
+    
     void Start()
     {
         health = maxHealth;
         player = GameObject.FindWithTag("Player").transform;
 
         rb = GetComponent<Rigidbody2D>();
+        gameManager = GameManager.Instance;
 
         // ignore collisions between player and enemy
-        Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), player.GetComponent<BoxCollider2D>());
+        Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), player.GetComponent<BoxCollider2D>());        
     }
 
     void Update()
     {
-        // distance check
+        // distance check        
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
         if (distanceToPlayer <= visionRaidus && distanceToPlayer > minDistanceFromPlayer)
         {
             MoveTowardsPlayer();
         }
-        else if(distanceToPlayer <= minDistanceFromPlayer && !hasFired)
+        else 
         {
-            rb.velocity = Vector2.zero;
-            hasFired = true;
-            Invoke("FireShot", fireRate);            
+             rb.velocity = Vector2.zero;
+
+            if (distanceToPlayer <= minDistanceFromPlayer && !hasFired)
+            {
+                hasFired = true;
+                Invoke("FireShot", fireRate);
+            }
         }
 
+        //if (SolidTileBelow(Vector2.right))
+        //rb.AddForce(Vector2.right * walkSpeed * Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     public void TakeDamage(int amount)
@@ -63,9 +79,9 @@ public class EnemyController : MonoBehaviour
         Vector2 offset;
 
         if (transform.position.x < player.position.x)
-            offset = Vector2.right * bulletSpawnOffsetDistance;
+            offset = Vector2.right * offsetDistance;
         else
-            offset = Vector2.left * bulletSpawnOffsetDistance;
+            offset = Vector2.left * offsetDistance;
 
         offset.y = -0.2f;
 
@@ -86,10 +102,39 @@ public class EnemyController : MonoBehaviour
 
     private void MoveTowardsPlayer()
     {
-        if(transform.position.x > player.position.x)
-            rb.AddForce(Vector2.left * walkSpeed * Time.deltaTime);
-        else if(transform.position.x < player.position.x)
-            rb.AddForce(Vector2.right * walkSpeed * Time.deltaTime);
+        if (transform.position.x > player.position.x)
+        {
+            if (SolidTileBelow(Vector2.left)) // ground check
+            {                
+                rb.velocity = Vector2.left * walkSpeed;// * Time.deltaTime;
+                Debug.Log("Moving Left");
+            }
+            else
+                rb.velocity = Vector2.zero;
+        }
+        else if (transform.position.x < player.position.x)
+        {
+            if (SolidTileBelow(Vector2.right)) // ground check
+            {
+                rb.velocity = Vector2.right * walkSpeed;// * Time.deltaTime;
+                Debug.Log("Moving Right");
+            }
+            else
+                rb.velocity = Vector2.zero;
+        }
+    }
+    
+    // Checks if there is a solide tile below to the left or right of the enemy
+    private bool SolidTileBelow(Vector2 direction)
+    {
+        var raycast = Physics2D.Raycast((Vector2)transform.position + direction * offsetDistance, Vector2.down, 1.4f);        
+
+        if (raycast)
+        { 
+            return true;
+        }
+
+        return false;        
     }
 
     private void OnDrawGizmos()
@@ -98,6 +143,13 @@ public class EnemyController : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, visionRaidus);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, minDistanceFromPlayer);
+
+        // raycast points
+        Gizmos.color = Color.white;       
+        Gizmos.DrawCube((Vector2)transform.position + Vector2.left * offsetDistance + Vector2.down * 1.3f, 
+            new Vector3(0.2f, 0.2f, 0));
+        Gizmos.DrawCube((Vector2)transform.position + Vector2.right * offsetDistance + Vector2.down * 1.3f,
+            new Vector3(0.2f, 0.2f, 0));
     }
 
 }
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Tilemaps")]
+    public Tilemap backgroundTiles;
+    public Tilemap collisionTiles;
+    public Tilemap foregroundTiles;
+
     public GameObject gameOverDialog;
     public GameObject alertMessage;
 
     public float alertTime = 1.0f;
+
+    public static GameManager Instance { get; private set; } // singleton
+
+    private void Awake()
+    {
+        // If there is an instance, and it is not this one, delete it.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
